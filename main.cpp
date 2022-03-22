97e35087dc79a95482b569d1370c9eb3abb798b7
Catlynne Quarles <catlynne.quarles@sinclair.edu>
2022-03-21 20:27:46 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Catlynne Quarles
+// CIS 1202 R02
+// March 22, 2022
+// Source Code Management Assignment 1
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World" << endl;
+    return 0;
+    
+}
